b9472b86a52ac713e4bbd04c01b206e97ce1cdd0
Pablovass <faktory.fm@live.com>
2023-06-20 09:02:26 -0300

    Sistema de animación en Unity: Personaje principal, preparar animaciones)

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,6 +7,7 @@ public class PlayerMovement : MonoBehaviour
     public Rigidbody2D playerRigidbody2D;
     public float speed = .5f;
     public float jumpSpeed=300;
+    bool isGrounded = true;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,9 +18,21 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         playerRigidbody2D.velocity = new Vector2(Input.GetAxis("Horizontal")*speed,playerRigidbody2D.velocity.y);
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (isGrounded)
         {
-            playerRigidbody2D.AddForce(Vector2.up * jumpSpeed);
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                playerRigidbody2D.AddForce(Vector2.up * jumpSpeed);
+                isGrounded = false;
+            }
+        }
+        
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
         }
     }
 }
